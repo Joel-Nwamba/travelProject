c376b2fe6189187a20999e8c1fedcd715c09f49d
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-03-31 20:12:56 -0400

    first commit

---
+++ my-travel-app/src/index.js
@@ -0,0 +1,8 @@
+// React starter
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+    <h1>Hello, create react app, and this will be fun</h1>,
+    document.getElementById("root")
+)
