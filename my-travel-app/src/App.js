673a4365dd76340b0a913f45618cbb94fed46e52
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-04-05 07:14:01 -0400

    update card information

--- my-travel-app/src/App.js
+++ my-travel-app/src/App.js
@@ -1,11 +1,27 @@
 import React from "react"
 import Navbar from "./component/Navbar"
+import Card from "./component/Card"
+import data from "./data"
 import "./styles.css"
 
 export default function App() {
+    const cardValue = data.map(item => {
+        return(
+            <Card 
+                img={item.imageUrl}
+                title={item.title}
+                location={item.location}
+                map={item.googleMapsUrl}
+                startDate={item.startDate}
+                endDate={item.endDate}
+                description={item.description}
+            />
+        )
+    })
     return(
         <div>
             <Navbar />
+            {cardValue}
         </div>
     )
 }
--- my-travel-app/src/component/Card.js
+++ my-travel-app/src/component/Card.js
@@ -1,9 +1,14 @@
 import React from "react"
 
-export default function Cards() {
+export default function Cards(props) {
     return(
         <div>
-            
+            <img src={props.img} alt=""/>
+            <h3>{props.title}</h3>
+            <p>{props.location}</p>
+            <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
+            <p>{props.startDate} - {props.endDate}</p>
+            <p>{props.descriptions}</p>
         </div>
     )
 }
