ee0c6509c0975ad720cffc9b962dcfb027a5f08e
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-04-07 20:06:54 -0400

    style website

--- my-travel-app/src/component/Card.js
+++ my-travel-app/src/component/Card.js
@@ -2,13 +2,13 @@ import React from "react"
 
 export default function Cards(props) {
     return(
-        <div>
-            <img src={props.img} alt=""/>
+        <div className="main-card">
+            <img className="image--header" src={props.img} alt=""/>
             <h3>{props.title}</h3>
             <p>{props.location}</p>
             <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
             <p>{props.startDate} - {props.endDate}</p>
-            <p>{props.descriptions}</p>
+            <p>{props.description}</p>
         </div>
     )
 }
