db69dda30d54161fa87d7b4b2725b01fcbbae6d9
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-04-08 19:38:28 -0400

    styling app now

--- my-travel-app/src/component/Card.js
+++ my-travel-app/src/component/Card.js
@@ -4,11 +4,15 @@ export default function Cards(props) {
     return(
         <div className="main-card">
             <img className="image--header" src={props.img} alt=""/>
-            <h3>{props.title}</h3>
+            <div className="card--container">
+            <h3 className="title--header">{props.title}</h3>
+            <div className="card--location">
             <p>{props.location}</p>
             <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
             <p>{props.startDate} - {props.endDate}</p>
             <p>{props.description}</p>
+            </div>
+         </div>
         </div>
     )
 }
