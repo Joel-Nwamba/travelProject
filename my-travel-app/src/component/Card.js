53fc215da31150fdfcb8c65bed211b8cd7608fdc
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-04-12 08:47:12 -0400

    update grid main

--- my-travel-app/src/component/Card.js
+++ my-travel-app/src/component/Card.js
@@ -4,13 +4,17 @@ export default function Cards(props) {
     return(
         <div className="main-card">
             <img className="image--header" src={props.img} alt=""/>
+            <div className="header">
             <h3 className="title--header">{props.title}</h3>
+            </div>
             <div className="card-location-map">
             <p><i class="fa-solid fa-location-dot"></i>{props.location}</p>
             <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
             </div>
+            <div className="date-description">
             <p className="dates">{props.startDate} - {props.endDate}</p>
             <p className="description">{props.description}</p>
+            </div>
             </div>
     )
 }
