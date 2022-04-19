8eec76d50bcbcc00cba3de9435f2ba590ad34b99
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2022-04-19 07:49:32 -0400

    Re-adjust heading later on

--- my-travel-app/src/component/Card.js
+++ my-travel-app/src/component/Card.js
@@ -7,7 +7,7 @@ export default function Cards(props) {
             <h3 className="title--header">{props.title}</h3>
             <div className="card-location-map">
             <p className="location"><i class="fa-solid fa-location-dot"></i>{props.location}</p>
-            <p><a href={props.googleMapsUrl}>View on Google Maps</a></p>
+            <p className="maps--location"><a href={props.googleMapsUrl}>View on Google Maps</a></p>
             </div>
             <div className="date-description">
             <p className="dates">{props.startDate} - {props.endDate}</p>
